3b2d29dfebaf27dab55cffcc17fe5f0c2bc0e23b
KostraOne <guillaume.amangoua@gmail.com>
2020-09-21 15:58:44 +0200

    ajout de liens de doc

--- login-vue/src/store/index.js
+++ login-vue/src/store/index.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 import user from "./user";
 // import product from "./product";
-
 // READ THE DOC !!!
 // https://vuex.vuejs.org/fr/api/
 // https://vuex.vuejs.org/fr/api/#modules
+// TUTO UTILE ICI =>
+// https://www.youtube.com/watch?v=2CSr2vBApSI
+// https://www.youtube.com/watch?v=iw1eajzWQAM
 
 Vue.use(Vuex);
 
